3ce452a99837b12aefd2911990121672dfdc40b9
Krishna <Narayk1@labcorp.com>
2022-05-26 10:50:15 -0400

    Update push

--- src/test/java/PageObjects/LoginPage.java
+++ src/test/java/PageObjects/LoginPage.java
@@ -1,7 +1,6 @@
 package PageObjects;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import Utilities.BaseClass;
@@ -10,12 +9,6 @@ public class LoginPage extends BaseClass {
 
 	public LoginPage(WebDriver driver, WebDriverWait wait) {
 		super(driver, wait);
-		PageFactory.initElements(driver, this);
 	}
-
-	
-	
-
-	
 
 }
