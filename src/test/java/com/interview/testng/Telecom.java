c2f8c16d975c1fe61cb62a8fb94b963fe5f5b0a7
Krishna <Narayk1@labcorp.com>
2022-07-19 09:55:00 -0400

    Adding testng files

---
+++ src/test/java/com/interview/testng/Telecom.java
@@ -0,0 +1,15 @@
+package com.interview.testng;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Telecom {
+
+	@Test
+	public void test1() throws IOException {
+		Assert.assertTrue(true);
+	}
+
+}
